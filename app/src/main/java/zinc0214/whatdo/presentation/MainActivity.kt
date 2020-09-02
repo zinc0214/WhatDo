d82a39f02c03803e2df0d8f0d19d4805060db15e
zinc <zinc0214@gmail.com>
2020-09-02 23:09:39 +0900

    화면 하나로 합치고 파이어베이스 연동

--- app/src/main/java/zinc0214/whatdo/presentation/MainActivity.kt
+++ app/src/main/java/zinc0214/whatdo/presentation/MainActivity.kt
@@ -1,33 +1,79 @@
 package zinc0214.whatdo.presentation
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import zinc0214.whatdo.R
 import zinc0214.whatdo.data.CardInfo
+import zinc0214.whatdo.data.ViewType
+import zinc0214.whatdo.databinding.ActivityMainBinding
 import zinc0214.whatdo.util.setStatusBar
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ViewDataBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewType: ViewType
     private var infoList = ArrayList<CardInfo>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setUpView()
     }
 
     private fun setUpView() {
         val num = Random.nextInt(2)
-        if (num == 1) {
-            binding = DataBindingUtil.setContentView(this, R.layout.activity_main_blue)
+        viewType = if (num == 1) {
             setStatusBar(this@MainActivity, R.color.blueStart)
+            ViewType.BLUE
         } else {
-            binding = DataBindingUtil.setContentView(this, R.layout.activity_main_pink)
             setStatusBar(this@MainActivity, R.color.pinkStart)
+            ViewType.PINK
         }
+
+        with(binding) {
+            type = viewType
+            activity = this@MainActivity
+        }
+
+        loadContent()
     }
 
+    private fun loadContent() {
+        val database = Firebase.database.reference
+        val contentDB = database.child("ko")
+        // Read from the database
+        contentDB.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("ayhan", "error : $error")
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue<ArrayList<CardInfo?>>()
+                value?.let {
+                    for (info in value) {
+                        info?.let { infoList.add(info) }
+                    }
+                }
+                Log.e("ayhan", "infoListSize : ${infoList.size}")
+            }
+        })
+    }
+
+    public fun startClickListener() {
+        val num = Random.nextInt(infoList.size - 1)
+        binding.cardInfo = infoList[num]
+        binding.notifyChange()
+        binding.homeGroup.visibility = View.GONE
+        binding.contentGroup.visibility = View.VISIBLE
+    }
 }
--- app/src/main/java/zinc0214/whatdo/presentation/SplashActivity.kt
+++ app/src/main/java/zinc0214/whatdo/presentation/SplashActivity.kt
@@ -3,14 +3,11 @@ package zinc0214.whatdo.presentation
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import kotlinx.coroutines.*
 import zinc0214.whatdo.R
 import zinc0214.whatdo.databinding.ActivitySplashBinding
-import java.util.concurrent.Delayed
 
 
 class SplashActivity : AppCompatActivity() {
@@ -28,13 +25,11 @@ class SplashActivity : AppCompatActivity() {
         }
     }
 
-
     private fun startMain() {
         val i = Intent(this, MainActivity::class.java)
 
         val transitionActivityOptions =
             ActivityOptions.makeSceneTransitionAnimation(this, binding.textLogo, "logo")
         startActivity(i, transitionActivityOptions.toBundle())
-
     }
 }
